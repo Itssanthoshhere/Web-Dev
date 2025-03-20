f513f14c20c12e640e9af3be46d9ad5db1d54d44
Itssanthoshhere <santhosh02vs@gmail.com>
2025-03-20 22:35:15 +0530

    Day - 61

---
+++ Web Dev-learning-journey/Day61 - Exercise - 13/script.js
@@ -0,0 +1,30 @@
+// Exercise - 13 | Dynamic Website Builder 
+
+console.log("Script.js initializing!!");
+
+createCard(
+    "Installing VS Code & How Websites Work | Web Development Course - Tutorial #1",
+    "CodeWithHarry",
+    1170000,
+    10,
+    "31:20",
+    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg"
+);
+
+createCard(
+    "Introduction to Frontend | Web Development Course - Tutorial #2",
+    "CodeWithHarry",
+    727000,
+    7,
+    "31:06",
+    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg"
+);
+
+createCard(
+    "Introduction to HTML | Web Development Course - Tutorial #3",
+    "CodeWithHarry",
+    560000,
+    2,
+    "12:23",
+    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg"
+);
