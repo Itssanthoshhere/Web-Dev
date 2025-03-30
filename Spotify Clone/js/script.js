c76bf44338c85a936e27085c0b455687412cf513
Itssanthoshhere <santhosh02vs@gmail.com>
2025-03-31 01:59:49 +0530

    Spotify Clone

--- Spotify Clone/js/script.js
+++ Spotify Clone/js/script.js
@@ -13,7 +13,7 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href);
+            songs.push(element.href.split("/songs/")[1]);
         }
     }
 
@@ -25,6 +25,22 @@ async function main() {
     let songs = await getSongs();
     console.log(songs);
 
+    // Show all the songs in the playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    songUL.innerHTML = ""
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>Sandy</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg" alt="">
+                            </div> </li>`;
+    }
+
+    /*
     // Play the first song
     var audio = new Audio(songs[0]);
     audio.play();
@@ -34,6 +50,7 @@ async function main() {
         console.log(duration);
         // The duration variable now holds the duration (in seconds) of the audio clip
     });
+    */
 }
 
 main();
