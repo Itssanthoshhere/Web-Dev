af7aee010e02ba40425ed3df980b19186aac7863
Itssanthoshhere <santhosh02vs@gmail.com>
2025-05-21 17:15:22 +0530

    Initial Commit

--- Web Dev-learning-journey/Day76 - ejs Template Engine in Express/main.js
+++ Web Dev-learning-journey/Day76 - ejs Template Engine in Express/main.js
@@ -1,27 +1,45 @@
+// NOTE: Importing required modules
 const express = require("express");
 const app = express();
 const port = 3000;
 
-// Using EJS with Express - https://github.com/mde/ejs/wiki/Using-EJS-with-Express
+// -----------------------------
+// EJS TEMPLATE ENGINE SETUP
+// -----------------------------
 
+// TODO: Set 'ejs' as the view engine
+// INFO: This tells Express to look for .ejs files in the 'views' folder
 app.set("view engine", "ejs");
 
+// -----------------------------
+// ROUTES
+// -----------------------------
+
+// ROUTE: Home Page
 app.get("/", (req, res) => {
+    // Data to send to EJS template
     let siteName = "Adidas";
     let searchText = "Search Now";
     let arr = ["Hey", 54, 65];
+
+    // Renders 'views/index.ejs' with provided data
     res.render("index", { siteName: siteName, searchText: searchText, arr: arr });
 });
 
+// ROUTE: Blog Post Page with dynamic slug
 app.get("/blog/:slug", (req, res) => {
+    // Example blog data
     let blogTitle = "Adidas why and when?";
-    let blogContent = "Its a great brand and its products are amazing.";
+    let blogContent = "It's a great brand and its products are amazing.";
+
+    // Renders 'views/blogpost.ejs' with dynamic content
     res.render("blogpost", { blogTitle: blogTitle, blogContent: blogContent });
 });
 
+// -----------------------------
+// SERVER LISTENING
+// -----------------------------
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-
-
