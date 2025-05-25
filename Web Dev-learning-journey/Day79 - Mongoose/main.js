a9b0e828019d1d475f9fbd04eafd34134301ef5e
Itssanthoshhere <santhosh02vs@gmail.com>
2025-05-25 16:10:32 +0530

    Initial Commit

--- Web Dev-learning-journey/Day79 - Mongoose/main.js
+++ Web Dev-learning-journey/Day79 - Mongoose/main.js
@@ -1,46 +1,67 @@
+
 // https://www.npmjs.com/package/mongodb
 
-// server.js
+// =======================
+// IMPORTS
+// =======================
 
-// Importing necessary modules
+// INFO: Importing required modules
 import mongoose from 'mongoose';
 import express from 'express';
 import { Todo } from "./models/Todo.js"; // Importing Todo model
 
-// Connecting to MongoDB
+// =======================
+// DATABASE CONNECTION
+// =======================
+
+// TODO: Connect to MongoDB (Ensure MongoDB server is running)
 let conn = await mongoose.connect("mongodb://localhost:27017/todo");
 
-// Creating an Express app
+// =======================
+// EXPRESS SERVER SETUP
+// =======================
+
+// INFO: Creating an Express application
 const app = express();
 const port = 3000;
 
-// Route to create and save a new Todo item
+// =======================
+// ROUTES
+// =======================
+
+// NOTE: Root route to create and save a new Todo item
 app.get('/', (req, res) => {
-    // Creating a new Todo object
+    // INFO: Creating a new Todo object
     const todo = new Todo({
+        // TODO: Add a title field if required
         // title: 'Learn Mongoose',
         desc: 'Learn how to use Mongoose with MongoDB',
         isDone: false,
-        days: Math.floor(Math.random() * 45 + 5 * Math.random())
+        days: Math.floor(Math.random() * 45 + 5 * Math.random()) // Random number of days
     });
 
-    // Saving the Todo object to MongoDB
+    // INFO: Saving the Todo to MongoDB
     todo.save();
 
-    // Sending a response
+    // INFO: Sending a simple response
     res.send('Hello World!');
 });
 
-
+// NOTE: Route to fetch one Todo from the database
 app.get('/a', async (req, res) => {
-    // Fetching all Todo items from MongoDB
+    // INFO: Fetching one Todo item from the collection
     let todo = await Todo.findOne({});
     console.log(todo);
-    res.json({title: todo.title, desc: todo.desc});
+
+    // INFO: Returning title and description as JSON
+    res.json({ title: todo.title, desc: todo.desc });
 });
 
-// Starting the Express server
+// =======================
+// SERVER START
+// =======================
+
+// INFO: Start the server on port 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-
--- Web Dev-learning-journey/Day79 - Mongoose/models/Todo.js
+++ Web Dev-learning-journey/Day79 - Mongoose/models/Todo.js
@@ -1,15 +1,34 @@
 // models/Todo.js
 
-// Importing Mongoose
+// =======================
+// IMPORTS
+// =======================
+
+// INFO: Importing Mongoose to define schema and model
 import mongoose from 'mongoose';
 
-// Defining the Todo schema
+// =======================
+// SCHEMA DEFINITION
+// =======================
+
+// NOTE: Defining the Todo schema
 const todoSchema = new mongoose.Schema({
-    title: {type: String, required: true, default: 'Hey'},
-    desc: String,
-    isDone: Boolean,
-    days: Number,
+    title: {
+        type: String,
+        required: true,
+        default: 'Hey' // NOTE: Default value if none provided
+    },
+    desc: String,      // INFO: Description of the Todo task
+    isDone: Boolean,   // INFO: Status - true if task is completed
+    days: Number       // INFO: Estimated or actual duration in days
 });
 
-// Creating and exporting the Todo model
+// =======================
+// MODEL EXPORT
+// =======================
+
+// INFO: Creating and exporting the Todo model based on the schema
 export const Todo = mongoose.model('Todo', todoSchema);
+// INFO: The model is used to interact with the MongoDB database
+// INFO: It allows us to create, read, update, and delete Todo items
+// INFO: The model name 'Todo' is used to create a collection named 'todos' in the database
